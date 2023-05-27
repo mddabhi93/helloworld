3c8187c8ad1d720ca8c56de73a788244642cc00f
Shubham Agrawal <shub8968@gmail.com>
2023-05-27 17:27:14 +0530

    update app.py

--- web/app/app.py
+++ web/app/app.py
@@ -12,4 +12,4 @@ redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Hello Container World! I have been seen %s times and my hostname is %s.\n' % (redis.get('hits'),socket.gethostname())
+    return 'Hello Containers World! I have been seen %s times and my hostname is %s.\n' % (redis.get('hits'),socket.gethostname())
